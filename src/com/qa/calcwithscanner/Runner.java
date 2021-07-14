c9a2f50085e4240672b13e9b1350a9fb1ac48629
Vinesh Ghela <vineshghela@gmail.com>
2021-07-14 11:04:45 +0100

    added calc

---
+++ src/com/qa/calcwithscanner/Runner.java
@@ -0,0 +1,13 @@
+package com.qa.calcwithscanner;
+
+import com.qa.calculatortask.Menu;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		Menu menu = new Menu();
+		menu.start();
+
+	}
+
+}
